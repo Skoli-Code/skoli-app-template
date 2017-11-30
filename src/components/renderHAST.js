b92b6dd785073293a69c5e125d6106962d2f0df2
pbellon <bellon.pierre@gmail.com>
2017-11-30 14:35:01 +0100

    Add inspiration link in renderHAST

--- src/components/renderHAST.js
+++ src/components/renderHAST.js
@@ -1,6 +1,7 @@
 import React, { createElement } from 'react'
 import PropTypes from 'prop-types' 
 
+// inspired by https://github.com/phenomic/phenomic/blob/69e6f8852e873d995a0c500182be373310befebe/packages/plugin-renderer-react/src/components/BodyRenderer.js
 const renderAST = ({ hast, componentsMap }) => {
   const renderTag = ({ type, tagName, properties, value, children=[]}) => {
     let elem
